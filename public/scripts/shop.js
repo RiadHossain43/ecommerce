ef917d8aa1b2209ad40dc3282665f706c5a34dee
RiadHossain <riadhossain7464@gmail.com>
2020-05-21 16:55:53 +0600

    90% update

--- public/scripts/shop.js
+++ public/scripts/shop.js
@@ -1,35 +1,45 @@
 import * as navigation from './navigation.js'
 import * as util from './util.js'
 import * as prod from './product_anim.js'
-// let prod_sec_Anim = () => {
-//     let products = util.eleCls('product');
+
+let handler_catagory_bar = () => {
+    let catagory_menu = util.eleID('catagory_menu')
+    let catagories = util.eleCls('catagory')
+    let sub_catagories = util.eleCls('sub_catagory')
+
+    if (window.innerWidth < 992) {
+        catagory_menu.addEventListener('click', () => {
+            for (let i = 0; i < catagories.length; i++) {
+                catagories[i].classList.toggle('de_activate_catagory_mobile')
+            }
+        })
+        for (let i = 0; i < catagories.length; i++) {
+            catagories[i].addEventListener('click', () => {
+                sub_catagories[i].classList.toggle('de_activate_catagory_mobile')
+            })
+        }
+    }
+}
+let habdle_filter_bar = () => {
+    let filter_mob_view = util.eleCls('activate_mobile_filter_view')
+    let filter_menu = util.eleID('filter_menu')
+    let hide_filter_bar = util.eleID('hide_filter_bar')
     
-//     let detail = util.eleCls('detail');
-//     let dt = util.eleCls('dt');
-    
-//     for (let i = 0; i < products.length; i++) {
-//         let product = products[i].getBoundingClientRect();
-
-//         if (product.top <= window.innerHeight * .75) {
-//             util.set_style(products[i], { opacity: 1, animation: 'fadeInUp .75s' })
-//         }
-
-//         if (window.innerWidth <= 768) {
-//             if (product.top >= 0 && product.bottom <= (window.innerHeight * 1.15 || document.documentElement.clientHeight * 1.15)) {
-//                 util.set_style(detail[i], { width: '75%', height: '75%', opacity: '.6' });
-//                 util.set_style(dt[i], { padding: '0px' });
-//             }
-//             else {
-//                 util.set_style(detail[i], { width: '95%', height: '95%', opacity: '0' });
-//                 util.set_style(dt[i], { padding: '20px 0px' });
-//             }
-//         }
-//     }
-// }
-
+    if (window.innerWidth < 992) {
+        filter_menu.addEventListener('click', () => {
+            filter_mob_view[0].classList.add('filter_view_slide_inout')
+        })
+        hide_filter_bar.addEventListener('click',()=>{
+            filter_mob_view[0].classList.remove('filter_view_slide_inout')
+        })
+        
+    }
+}
 window.addEventListener('scroll', () => {
     prod.prod_sec_Anim()
     navigation.animate();
 });
 
 navigation.handleMenu()
+handler_catagory_bar()
+habdle_filter_bar()
